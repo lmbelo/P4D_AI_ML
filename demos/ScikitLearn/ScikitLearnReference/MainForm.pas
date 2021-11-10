500bfe70386ff37e2d1d1dd9fa861da0c787eac9
lmbelo <lucas.belo@live.com>
2021-11-10 11:42:15 -0800

    Small adjustment

--- demos/ScikitLearn/ScikitLearnReference/MainForm.pas
+++ demos/ScikitLearn/ScikitLearnReference/MainForm.pas
@@ -6,7 +6,8 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, PythonEngine,
   FMX.PythonGUIInputOutput, FMX.Memo.Types, FMX.Controls.Presentation,
-  FMX.ScrollBox, FMX.Memo, PyCommon, PyModule, ScikitLearn;
+  FMX.ScrollBox, FMX.Memo, PyCommon, PyModule, ScikitLearn, FMX.StdCtrls,
+  FMX.Layouts, MatplotLib, NumPy, PyPackage;
 
 type
   TForm1 = class(TForm)
@@ -14,8 +15,15 @@ type
     PythonGUIInputOutput1: TPythonGUIInputOutput;
     Memo1: TMemo;
     ScikitLearn1: TScikitLearn;
+    Layout1: TLayout;
+    Button1: TButton;
+    GroupBox1: TGroupBox;
+    rbDecisionTreeRegression: TRadioButton;
+    NumPy1: TNumPy;
+    MatplotLib1: TMatplotLib;
+    procedure Button1Click(Sender: TObject);
   private
-    { Private declarations }
+    procedure DoDecisionTreeRegression();
   public
     { Public declarations }
   end;
@@ -26,5 +34,19 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if rbDecisionTreeRegression.IsChecked then begin
+    DoDecisionTreeRegression();
+  end;
+end;
+
+procedure TForm1.DoDecisionTreeRegression;
+begin
+  with ScikitLearn1, NumPy1, MatplotLib1 do begin
+    sklearn.tree;
+  end;
+end;
 
 end.
